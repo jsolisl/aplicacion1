345df871b2a79f8b74f0ee0f58df97281e4f317c
jsolisl <javiersolislastra@gmail.com>
2022-07-26 00:54:52 +0000

    Este archivo si corre todo ok

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_templates 
+from flask import Flask, render_template 
 app = Flask("hola")
 # definimos ruta-raiz
 @app.route("/")
 def hola():
-    return "Hola Mundo, paso inicial 2789"
+    return "Hola Mundo, paso inicial 278913677777"
 
 @app.route("/meucontato")
 def meucontato():
-    return render_templates("index.html")
+    return render_template("index.html")
 
