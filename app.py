366addc178c1fd3d96863389250ca8ebf425de9d
jsolisl <javiersolislastra@gmail.com>
2022-07-27 00:32:13 +0000

    se creo html y css

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
 from flask import Flask, render_template 
+from datetime import datetime
 app = Flask("hola")
+
+posts = [
+    {
+        "title": "Mi primer Post",
+        "body": "Aqui o texto do Post",
+        "author": "Feulo",
+        "created":datetime(2022,7,25)
+    },
+     {
+        "title": "Mi segundo Post",
+        "body": "Aqui o texto do Post",
+        "author": "Danilo",
+        "created":datetime(2022,7,26)
+    }
+]
+
 # definimos ruta-raiz
 @app.route("/")
 def hola():
-    return "Hola Mundo, paso inicial 278913677777"
+    return render_template("index.html", posts = posts)
 
-@app.route("/meucontato")
-def meucontato():
-    return render_template("index.html")
+#@app.route('/login')
 
